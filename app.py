7b0b27a7e96bc40a8bc182b3a73029c518ece9ca
Interpreter <cybergates@live.cybergates>
2026-07-14 00:46:21 +0300

    op

--- app.py
+++ app.py
@@ -22,9 +22,9 @@ logging.getLogger('prophet').setLevel(logging.WARNING)
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 try:
-    mauzo_global = conn.read(worksheet="mauzo")
-    stoo_global = conn.read(worksheet="stoo")
-    orders_global = conn.read(worksheet="orders")
+    mauzo_global = conn.read(worksheet="mauzo", ttl=0)
+    stoo_global = conn.read(worksheet="stoo", ttl0)
+    orders_global = conn.read(worksheet="orders", ttl0)
 except Exception as e:
     st.error(f"Hitilafu ya Mtandao: {e}")
     mauzo_global = pd.DataFrame(columns=['Date', 'Category', 'Qty', 'Price', 'Total', 'Profit'])
