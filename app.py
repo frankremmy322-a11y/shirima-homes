411d1ede9c158106cf9233834c6188626d0bc14f
Interpreter <cybergates@live.cybergates>
2026-07-14 12:33:47 +0300

    uoi

--- app.py
+++ app.py
@@ -913,7 +913,7 @@ if check_password():
           if st.form_submit_button("Hifadhi Oda"):
            if mteja_mpya and simu_mpya and bidhaa_final:
              mpya={
-                'Tarehe':datetime.datetime.now().strftime("%Y-%m-%d"),
+                'Tarehe':datetime.datetime.now().strftime("%d/%m/%Y"),
                 'Mteja':mteja_mpya,
                 'Simu':str(simu_mpya),
                 'Bidhaa':bidhaa_final,
