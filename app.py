1f17dd4ff6a16c736457cc0de7a33a05fa903937
Interpreter <cybergates@live.cybergates>
2026-09-21 10:06:44 +0300

    gcff0657hghgjjbjhj

--- app.py
+++ app.py
@@ -156,6 +156,8 @@ def image_to_base64(image_path):
 def password_entered():
    if st.session_state.get("password")== st.secrets.get("PASSCODE"):
     st.session_state["password_correct"]=True
+    st.session_state["password"] = ""
+    st.rerun()
     #del st.session_state["password"]
    else:
     st.session_state["password_correct"]=False
